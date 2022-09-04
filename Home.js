21d2f1b071925622762790b5d80a7a89b462ea3b
test <test@test.com>
2022-09-04 18:22:31 -0300

    Changed navbar and added a hamburger menu for mobile

---
+++ Home.js
@@ -0,0 +1,10 @@
+// Mobile hamburger menu
+const hamburgerMenu = document.querySelector(".hamburger");
+const hamburgerMenuDropdown = document.querySelector(".dropdown");
+
+hamburgerMenu.addEventListener("click", function () {
+  this.classList.toggle("is-active");
+  hamburgerMenuDropdown.classList.toggle("is-active");
+});
+
+//==================================================
