6415aad845267f7a0c1ffdd2d09bc6a69d88c7c5
test <test@test.com>
2022-09-14 11:46:39 -0300

    Sliders remake and size adjustments

--- Home.js
+++ Home.js
@@ -1,4 +1,6 @@
-// Mobile hamburger menu
+//==================================================
+// MOBILE HAMBURGER MENU
+
 const hamburgerMenu = document.querySelector(".hamburger");
 const hamburgerMenuDropdown = document.querySelector(".dropdown");
 
@@ -6,25 +8,105 @@ hamburgerMenu.addEventListener("click", function () {
   this.classList.toggle("is-active");
   hamburgerMenuDropdown.classList.toggle("is-active");
 });
-
 //==================================================
 
-//Announcements slider
-const buttons = document.querySelectorAll("[data-carousel-button]");
+//==================================================
+//ANNOUNCEMENTS SLIDER
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-    const slides = button
-      .closest("[data-carousel]")
-      .querySelector("[data-slides]");
+//Set starting slide
+let announcementsSlideIndex = 1;
+showAnnouncementSlides(announcementsSlideIndex);
 
-    const activeSlide = slides.querySelector("[data-active]");
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-    if (newIndex < 0) newIndex = slides.children.length - 1;
-    if (newIndex >= slides.children.length) newIndex = 0;
+// Next/previous slide navigation (buttons)
+function changeAnnouncementSlide(announcementsStartingSlide) {
+  showAnnouncementSlides(
+    (announcementsSlideIndex += announcementsStartingSlide)
+  );
+}
 
-    slides.children[newIndex].dataset.active = true;
-    delete activeSlide.dataset.active;
-  });
-});
+// Thumbnail image controls
+function currentAnnouncementSlide(announcementsStartingSlide) {
+  showAnnouncementSlides(
+    (announcementsSlideIndex = announcementsStartingSlide)
+  );
+}
+
+function showAnnouncementSlides(announcementsStartingSlide) {
+  let i;
+  let slides = document.getElementsByClassName("announcements-slides");
+  let navigationDots = document.getElementsByClassName(
+    "announcements-dot-navigation"
+  );
+  //Defaults to first slide if the announcementsSlideIndex is higher than the amount of slides
+  if (announcementsStartingSlide > slides.length) {
+    announcementsSlideIndex = 1;
+  }
+  //Defaults to last slide if the announcementsSlideIndex is lower than the amount of slides
+  if (announcementsStartingSlide < 1) {
+    announcementsSlideIndex = slides.length;
+  }
+  //Hides all slides
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  //Removes active class from the dot navigation
+  for (i = 0; i < navigationDots.length; i++) {
+    navigationDots[i].className = navigationDots[i].className.replace(
+      " announcements-active",
+      ""
+    );
+  }
+  //Shows current slide
+  slides[announcementsSlideIndex - 1].style.display = "block";
+  //Activates corresponding dot navigation based on current slide
+  navigationDots[announcementsSlideIndex - 1].className +=
+    " announcements-active";
+}
+//==================================================
+
+//==================================================
+//NEWS SLIDER
+
+//Set starting slide
+let newsSlideIndex = 1;
+showNewsSlides(newsSlideIndex);
+
+// Next/previous slide navigation (buttons)
+function changeNewsSlide(newsStartingSlide) {
+  showNewsSlides((newsSlideIndex += newsStartingSlide));
+}
+
+// Thumbnail image controls
+function currentNewsSlide(newsStartingSlide) {
+  showNewsSlides((newsSlideIndex = newsStartingSlide));
+}
+
+function showNewsSlides(newsStartingSlide) {
+  let i;
+  let slides = document.getElementsByClassName("news-slides");
+  let navigationDots = document.getElementsByClassName("news-dot-navigation");
+  //Defaults to first slide if the newsSlideIndex is higher than the amount of slides
+  if (newsStartingSlide > slides.length) {
+    newsSlideIndex = 1;
+  }
+  //Defaults to last slide if the newsSlideIndex is lower than the amount of slides
+  if (newsStartingSlide < 1) {
+    newsSlideIndex = slides.length;
+  }
+  //Hides all slides
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  //Removes active class from the dot navigation
+  for (i = 0; i < navigationDots.length; i++) {
+    navigationDots[i].className = navigationDots[i].className.replace(
+      " news-active",
+      ""
+    );
+  }
+  //Shows current slide
+  slides[newsSlideIndex - 1].style.display = "block";
+  //Activates corresponding dot navigation based on current slide
+  navigationDots[newsSlideIndex - 1].className += " news-active";
+}
+//==================================================
